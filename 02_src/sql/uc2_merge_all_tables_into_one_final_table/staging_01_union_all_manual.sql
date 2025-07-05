98725f7783648be43864d3be450741d11ac59915
Steven MANIPHANH <zazo77600@gmail.com>
2025-07-05 12:56:51 +0200

    Modification architecture

--- 02_src/sql/uc2_merge_all_tables_into_one_final_table/staging_01_union_all_manual.sql
+++ 02_src/sql/uc2_merge_all_tables_into_one_final_table/staging_01_union_all_manual.sql
@@ -1,49 +1,31 @@
 CREATE OR REPLACE TABLE `covid19-1-463112.staging_dataset_2_covid19_our_world.covid19_cases_deaths_2020`
 PARTITION BY date
 CLUSTER BY country AS
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_africa`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_africa`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_americas`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_americas`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_eastern_mediterranean`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_eastern_mediterranean`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_europe`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_europe`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_south_east_asia`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_south_east_asia`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_western_pacific`;
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2020_western_pacific`;
 
+-----
 
---
-
-CREATE OR REPLACE TABLE `covid19-1-463112.staging_dataset_2_covid19_our_world.covid19_cases_deaths_2020`
+CREATE OR REPLACE TABLE `covid19-1-463112.staging_dataset_2_covid19_our_world.covid19_cases_deaths_2021`
 PARTITION BY date
 CLUSTER BY country AS
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_africa`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_africa`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_americas`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_americas`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_eastern_mediterranean`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_eastern_mediterranean`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_europe`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_europe`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_south_east_asia`
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_south_east_asia`
 UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_western_pacific`;
-
---
-
-CREATE OR REPLACE TABLE `covid19-1-463112.staging_dataset_2_covid19_our_world.covid19_cases_deaths_2020`
-PARTITION BY date
-CLUSTER BY country AS
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_africa`
-UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_americas`
-UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_eastern_mediterranean`
-UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_europe`
-UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_south_east_asia`
-UNION ALL
-SELECT * FROM `covid19-1-463112.dataset_2_covid19_our_world.cases_deaths_2020_western_pacific`;
+SELECT * FROM `covid19-1-463112.raw_dataset_2_covid19_our_world.cases_deaths_2021_western_pacific`;

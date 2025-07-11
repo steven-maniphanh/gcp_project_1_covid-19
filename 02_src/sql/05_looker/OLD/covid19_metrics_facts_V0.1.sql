6419616e8c24951b37e1b58d8f57db075a286899
Steven MANIPHANH <zazo77600@gmail.com>
2025-07-12 00:45:41 +0200

    Update of SQL queries for creation of Datasets Raw/Staged/Curated/Looker

---
+++ 02_src/sql/05_looker/OLD/covid19_metrics_facts_V0.1.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE TABLE covid19-1-463112.looker_dataset_2_covid19_our_world.covid19_metrics_facts
+PARTITION BY date
+CLUSTER BY country AS
+
+WITH CTE AS(
+SELECT
+  c1.country,
+  c1.country_code,
+  c1.region,
+  c1.continent,
+  c1.date,
+  EXTRACT(YEAR FROM date) AS year,
+  FORMAT_DATE('%Y-%m',date) AS year_month,
+  c1.new_cases,
+  c1.total_cases,
+  c1.new_deaths,
+  c1.total_deaths,
+  c2.population,
+  c2.area_km2,
+  c2.density_km2
+FROM `covid19-1-463112.curated_dataset_2_covid19_our_world.covid19_cases_deaths_full_data_final` as c1
+LEFT JOIN `covid19-1-463112.staging_tranverse_dataset.country_population_facts` as c2
+ON c1.country_code = c2.country_code
+WHERE c2.year = EXTRACT(YEAR FROM date)
+ORDER BY country, date)
+
+SELECT 
+  country,
+  country_code,
+  region,
+  continent,
+  year,
+  year_month,
+  date,
+  new_cases,
+  total_cases,
+  new_deaths,
+  total_deaths,
+  population,
+  -- Metrics calculated
+  ROUND(SAFE_DIVIDE(new_cases, population),6) * 1000000 AS new_cases_per_million,
+  ROUND(SAFE_DIVIDE(new_deaths, population),6) * 1000000 AS new_deaths_per_million,
+  ROUND(SAFE_DIVIDE(total_cases, population),6) * 1000000 AS total_cases_per_million,
+  ROUND(SAFE_DIVIDE(total_deaths, population),6) * 1000000 AS total_deaths_per_million,
+  ROUND(SAFE_DIVIDE(total_deaths, total_cases),4) * 100 AS mortality_rate_pct,
+  ROUND(SAFE_DIVIDE(total_cases, population),4) * 100 AS infection_rate_pct,
+  SUM(new_cases) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_new_cases,
+  SUM(new_deaths) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_new_death,
+  SUM(new_cases) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS monthly_new_cases,
+  SUM(new_deaths) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS monthly_new_deaths,
+  AVG(new_cases) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg_new_cases,
+  AVG(new_deaths) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS weekly_avg_new_deaths,
+  AVG(new_cases) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS monthly_avg_new_cases,
+  AVG(new_deaths) OVER (PARTITION BY country ORDER BY date ROWS BETWEEN 29 PRECEDING AND CURRENT ROW) AS monthly_avg_new_deaths,
+FROM CTE
